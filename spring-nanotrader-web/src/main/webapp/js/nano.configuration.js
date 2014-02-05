e576042269dd7090ae4821f058cd2df0090b87ec
Cornelia Davis <cornelia@corneliadavis.com>
2014-02-04 21:00:15 -0800

    make deployable to cloud foundry
    This mainly involves:
    * pull web tier into (RESTful) services tier (temporary to avoid cross-site scripting issues)
    * add spring-cloud to facilitate service binding
    * configure hibernate to use mysql dialect

--- spring-nanotrader-web/src/main/webapp/js/nano.configuration.js
+++ spring-nanotrader-web/src/main/webapp/js/nano.configuration.js
@@ -5,7 +5,8 @@
 nano.conf = {
     device : 'computer',                            // Device rendering the application (changes to "mobile" depending on the user agent)
     sessionCookieName : 'nanotraderSession',        // Name of the Cookie that will store the session info in the browser
-    urlRoot : '/spring-nanotrader-services/api/',   // Path to the API service
+//    urlRoot : '/spring-nanotrader-services/api/',   // Path to the API service
+    urlRoot : '/api/',   // Path to the API service - when deployed to CF 
     tplRoot : './templates/',                       // Path to the Templates directory
     accountIdUrlKey : '{accountid}',                // Key in the api urls that's gonna be replaced with the actual accountid
     pageUrlKey : '{page}',                          // Key in the api urls that's gonna be replaced with the page number
