7454ce270b53a62d12aded76b1284b695056382d
JaredGordon <jgordon@pivotal.io>
2015-05-22 14:10:12 -0400

    Builds, tests, deploys, and runs using jdk7.

--- deployApp.sh
+++ deployApp.sh
@@ -18,7 +18,7 @@ cf push -p dist/spring-nanotrader-services-1.0.1.BUILD-SNAPSHOT.war -m 1G -t 180
 cf bind-service $frontName $sqlName
 cf bind-service $frontName $messagingName
 cf set-env $frontName JBP_CONFIG_OPEN_JDK_JRE '[version: 1.7.0_+]'
-cf set-env $frontName JBP_CONFIG_TOMCAT '[tomcat: {version: 6.0.+}]'
+cf set-env $frontName JBP_CONFIG_TOMCAT '[tomcat: {version: 7.0.+}]'
 cf push -p dist/spring-nanotrader-services-1.0.1.BUILD-SNAPSHOT.war -m 1G -t 180 -d $domain -n $frontName $frontName
 
 echo Making this app available as a service instance
@@ -27,14 +27,14 @@ cf cups $frontName -p '{"uri":"http://'$frontName'.'$domain'/api/"}'
 echo Deploying the web tier
 cf push -p dist/spring-nanotrader-web-1.0.1.BUILD-SNAPSHOT.war -m 1G -t 180 -d $domain -n $webName --no-start $webName
 cf set-env $webName JBP_CONFIG_OPEN_JDK_JRE '[version: 1.7.0_+]'
-cf set-env $webName JBP_CONFIG_TOMCAT '[tomcat: {version: 6.0.+}]'
+cf set-env $webName JBP_CONFIG_TOMCAT '[tomcat: {version: 7.0.+}]'
 cf bind-service $webName $frontName
 cf push -p dist/spring-nanotrader-web-1.0.1.BUILD-SNAPSHOT.war -m 1G -t 180 -d $domain -n $webName $webName
 
 echo Deploying back end services tier
 cf push -p dist/spring-nanotrader-asynch-services-1.0.1.BUILD-SNAPSHOT.war -m 1G -t 180 -d $domain -n $backName --no-start $backName
 cf set-env $backName JBP_CONFIG_OPEN_JDK_JRE '[version: 1.7.0_+]'
-cf set-env $backName JBP_CONFIG_TOMCAT '[tomcat: {version: 6.0.+}]'
+cf set-env $backName JBP_CONFIG_TOMCAT '[tomcat: {version: 7.0.+}]'
 cf bind-service $backName $sqlName
 cf bind-service $backName $messagingName
 cf push -p dist/spring-nanotrader-asynch-services-1.0.1.BUILD-SNAPSHOT.war -m 1G -t 180 -d $domain -n $backName $backName
