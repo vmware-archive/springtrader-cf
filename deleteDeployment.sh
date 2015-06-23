7ff11b778a18b94e3c4a2bf16a743a8948f9fc3d
JaredGordon <jgordon@pivotal.io>
2015-06-23 17:07:27 -0400

    hook up user provided service.

--- deleteDeployment.sh
+++ deleteDeployment.sh
@@ -3,6 +3,7 @@
 frontName=traderfront
 webName=traderweb
 backName=traderback
+quoteName=quoteService
 domain=cfapps.io
 sqlName=tradersql
 messagingName=tradermessaging
@@ -13,8 +14,7 @@ cf delete -f $frontName
 cf delete -f $webName
 cf delete -f $backName
 cf delete-service -f $frontName
-cf delete-service -f $sqlName
-cf delete-service -f $messagingName
+cf delete-service -f $quoteName
 cf delete-route $domain -f -n $frontName
 cf delete-route $domain -f -n $webName
 cf delete-route $domain -f -n $backName
