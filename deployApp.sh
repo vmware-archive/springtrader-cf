5f3db3f224593a558d0daf8b50b9765e8b0a59fd
JaredGordon <jgordon@pivotal.io>
2015-08-07 17:18:05 -0400

    fix env value per new buildpack.

--- deployApp.sh
+++ deployApp.sh
@@ -17,7 +17,7 @@ echo Deploying front end services tier
 cf push -p dist/spring-nanotrader-services-1.0.1.BUILD-SNAPSHOT.war -m 1G -t 180 -d $domain -n $frontName --no-start $frontName
 cf bind-service $frontName $sqlName
 cf bind-service $frontName $messagingName
-cf set-env $frontName JBP_CONFIG_OPEN_JDK_JRE '[version: 1.7.0_+]'
+cf set-env $frontName JBP_CONFIG_OPEN_JDK_JRE '[jre: {version: 1.7.0_+}]'c
 cf set-env $frontName JBP_CONFIG_TOMCAT '[tomcat: {version: 7.0.+}]'
 cf push -p dist/spring-nanotrader-services-1.0.1.BUILD-SNAPSHOT.war -m 1G -t 180 -d $domain -n $frontName $frontName
 
@@ -26,14 +26,14 @@ cf cups $frontName -p '{"uri":"http://'$frontName'.'$domain'/api/"}'
 
 echo Deploying the web tier
 cf push -p dist/spring-nanotrader-web-1.0.1.BUILD-SNAPSHOT.war -m 1G -t 180 -d $domain -n $webName --no-start $webName
-cf set-env $webName JBP_CONFIG_OPEN_JDK_JRE '[version: 1.7.0_+]'
+cf set-env $webName JBP_CONFIG_OPEN_JDK_JRE '[jre: {version: 1.7.0_+}]'
 cf set-env $webName JBP_CONFIG_TOMCAT '[tomcat: {version: 7.0.+}]'
 cf bind-service $webName $frontName
 cf push -p dist/spring-nanotrader-web-1.0.1.BUILD-SNAPSHOT.war -m 1G -t 180 -d $domain -n $webName $webName
 
 echo Deploying back end services tier
 cf push -p dist/spring-nanotrader-asynch-services-1.0.1.BUILD-SNAPSHOT.war -m 1G -t 180 -d $domain -n $backName --no-start $backName
-cf set-env $backName JBP_CONFIG_OPEN_JDK_JRE '[version: 1.7.0_+]'
+cf set-env $backName JBP_CONFIG_OPEN_JDK_JRE '[jre: {version: 1.7.0_+}]'
 cf set-env $backName JBP_CONFIG_TOMCAT '[tomcat: {version: 7.0.+}]'
 cf bind-service $backName $sqlName
 cf bind-service $backName $messagingName
