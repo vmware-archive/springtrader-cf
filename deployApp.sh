4f917387ec294d4de7593e490c04ab054075a80d
JaredGordon <jgordon@pivotal.io>
2016-03-04 13:42:00 -0500

    run under jdk8.

--- deployApp.sh
+++ deployApp.sh
@@ -10,8 +10,6 @@ cf create-service cloudamqp lemur ${messagingName}
 echo Deploying front end services tier
 cf push -b java_buildpack -p dist/spring-nanotrader-services-${version}.war -m 1G -t 180 -d ${domain} -n ${frontName} --no-start ${frontName}
 cf bind-service ${frontName} ${messagingName}
-cf set-env ${frontName} JBP_CONFIG_OPEN_JDK_JRE '[jre: {version: 1.7.0_+}]'
-cf set-env ${frontName} JBP_CONFIG_TOMCAT '[tomcat: {version: 7.0.+}]'
 cf set-env ${frontName} LIVE_QUOTE_SERVICE_NAME ${liveQuoteServiceEurekaName}
 cf set-env ${frontName} DB_QUOTE_SERVICE_NAME ${dbQuoteServiceEurekaName}
 cf set-env ${frontName} ACCOUNT_SERVICE_NAME ${accountServiceEurekaName}
@@ -23,15 +21,11 @@ cf cups ${frontName} -p '{"uri":"http://'${frontName}'.'${domain}'/api/"}'
 
 echo Deploying the web tier
 cf push -b java_buildpack -p dist/spring-nanotrader-web-${version}.war -m 1G -t 180 -d ${domain} -n ${webName} --no-start ${webName}
-cf set-env ${webName} JBP_CONFIG_OPEN_JDK_JRE '[jre: {version: 1.7.0_+}]'
-cf set-env ${webName} JBP_CONFIG_TOMCAT '[tomcat: {version: 7.0.+}]'
 cf bind-service ${webName} ${frontName}
 cf push -b java_buildpack -p dist/spring-nanotrader-web-${version}.war -m 1G -t 180 -d ${domain} -n ${webName} ${webName}
 
 echo Deploying back end services tier
 cf push -b java_buildpack -p dist/spring-nanotrader-asynch-services-${version}.war -m 1G -t 180 -d ${domain} -n ${backName} --no-start ${backName}
-cf set-env ${backName} JBP_CONFIG_OPEN_JDK_JRE '[jre: {version: 1.7.0_+}]'
-cf set-env ${backName} JBP_CONFIG_TOMCAT '[tomcat: {version: 7.0.+}]'
 cf set-env ${backName} LIVE_QUOTE_SERVICE_NAME ${liveQuoteServiceEurekaName}
 cf set-env ${backName} DB_QUOTE_SERVICE_NAME ${dbQuoteServiceEurekaName}
 cf set-env ${backName} ACCOUNT_SERVICE_NAME ${accountServiceEurekaName}
